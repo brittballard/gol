91be5f429e0baeb3e67c688906e34b2beeb2a7a9
Britt Ballard <britt@thoughtbot.com>
2013-06-30 15:17:08 -0700

    Built a mock in the test.

--- cell_test.go
+++ cell_test.go
@@ -2,7 +2,15 @@ package gol
 
 import "testing"
 
-func TestIsAlive(t *testing.T) {
+type testSwaper struct {
+  callCount int
+}
+
+func (t *testSwaper) SwapXAndY() {
+  t.callCount++
+}
+
+func TestSwapXAndY(t *testing.T) {
   c := Cell{x: 1, y: 2}
   c.SwapXAndY()
   if c.x == 1 {
@@ -14,6 +22,14 @@ func TestDoTheSwap(t *testing.T) {
   c := &Cell{x: 5, y: 4}
   DoTheSwap(c)
   if c.x == 5 {
+    t.Error("No deal!")
+  }
+}
+
+func TestDoTheSwapWithMock(t *testing.T) {
+  ts := &testSwaper{}
+  DoTheSwap(ts)
+  if ts.callCount != 1 {
     t.Error("No deal!")
   }
 }
